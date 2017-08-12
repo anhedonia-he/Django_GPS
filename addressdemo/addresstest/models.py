88f355d7fd773c1a2abdd001c262d8ad7c715c67
贺诗淳 <anhedoniahe@gmail.com>
2017-08-12 21:36:17 +0800

    多定位点添加 √。

--- addressdemo/addresstest/models.py
+++ addressdemo/addresstest/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class address_info(models.Model):
     longitude = models.FloatField()
     latitude = models.FloatField()
-    data = models.CharField(max_length=200)
 
     def __str__(self):
         return self.longitude
--- addressdemo/addresstest/views.py
+++ addressdemo/addresstest/views.py
@@ -3,8 +3,6 @@ import json
 from addresstest.models import address_info
 
 def test(request):
-    address_point = address_info.objects.all()
-    address_data = []
     # for i in range(len(address_point)):
     #     address_longitude.append(address_point[i].longitude)
     #     address_latitude.append(address_point[i].latitude)
@@ -14,10 +12,25 @@ def test(request):
     #                'address_latitude': json.dumps(address_latitude), 'address_data': json.dumps(address_data)})
     if request.method == "POST":
         address = request.POST
-        # for address_longitude , address_latitude in address:
-        #     return render(request, 'address.html', {'address_longitude': address_longitude, 'address_latitude': address_latitude})
+        #向数据库添加 POST 来的点
         address_longitude = int(address.get("address_longitude"))
         address_latitude = int(address.get("address_latitude"))
-        return render(request, 'address.html', {'address_longitude': address_longitude, 'address_latitude': address_latitude})
+        address_info.objects.create(longitude = address_longitude, latitude = address_latitude)
+        #向模板传递数据点列表
+        address_point = address_info.objects.all()
+        address_longitude_list = []
+        address_latitude_list = []
+        for i in range(len(address_point)):
+            address_longitude_list.append(address_point[i].longitude)
+            address_latitude_list.append(address_point[i].latitude)
+        list(address_longitude_list)
+        list(address_latitude_list)
+        return render(request, 'address.html', {'address_longitude': address_longitude_list, 'address_latitude': address_latitude_list})
     else:
-        return render(request, 'address.html', {'address_longitude': 123, 'address_latitude': 47})
+        address_point = address_info.objects.all()
+        address_longitude_list = []
+        address_latitude_list = []
+        for i in range(len(address_point)):
+            address_longitude_list.append(address_point[i].longitude)
+            address_latitude_list.append(address_point[i].latitude)
+        return render(request, 'address.html', {'address_longitude': address_longitude_list, 'address_latitude': address_latitude_list})
